1acf7f5e4cf9e75813c9463819871167e77921bd
Alyssa D. Bautista <alyssarhenoanicolebautista@gmail.com>
2026-06-12 05:22:27 +0800

    Trying to use flutter run with android studio

--- flutter_application_1/lib/pages/social_viewer_page.dart
+++ flutter_application_1/lib/pages/social_viewer_page.dart
@@ -1,45 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import '../constants/colors.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import '../models/social.dart';
+import '../widgets/page_header.dart';
 
-class SocialViewerPage extends StatelessWidget {
+class SocialViewerPage extends StatefulWidget {
   final Social social;
 
-  const SocialViewerPage({
-    Key? key,
-    required this.social,
-  }) : super(key: key);
+  const SocialViewerPage({Key? key, required this.social}) : super(key: key);
+
+  @override
+  State<SocialViewerPage> createState() => _SocialViewerPageState();
+}
+
+class _SocialViewerPageState extends State<SocialViewerPage> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (_) => setState(() => _isLoading = true),
+          onPageFinished: (_) => setState(() => _isLoading = false),
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.social.webUrl));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(social.name),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Opening ${social.name}...',
-              style: const TextStyle(fontSize: 16),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          PageHeader(
+            onBackPressed: () => Navigator.pop(context),
+            title: widget.social.name,
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                WebViewWidget(controller: _controller),
+                if (_isLoading)
+                  const Center(child: CircularProgressIndicator()),
+              ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                if (await canLaunchUrl(Uri.parse(social.webUrl))) {
-                  await launchUrl(Uri.parse(social.webUrl));
-                }
-              },
-              child: const Text('Visit Website'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
